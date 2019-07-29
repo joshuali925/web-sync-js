143c7dc098690e0e426a48d06ebd63c4a1d5e6ea
Joshua Li <joshuali925@gmail.com>
2019-07-28 22:10:01 -0400

    fix alert, add clear button

--- static/client.js
+++ static/client.js
@@ -8,14 +8,34 @@ function wait(fn, ms) {
     }
 }
 
-function copy_all() {
-    $('#textarea').select();
+let copy_all = function () {
+    let elementId = 'textarea';
+    let input = document.getElementById(elementId);
+    if (navigator.userAgent.match(/ipad|iphone/i)) {  // for iOS
+        let range = document.createRange();
+        range.selectNodeContents(input);
+        let selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        input.setSelectionRange(0, 999999);
+    } else {
+        input.select();
+    }
     document.execCommand('copy');
-    show_alert('Copied!');
-}
+    $('#copybutton').focus();
+    show_alert('All copied!');
+};
+
+let clear_all = () => {
+    $('#textarea').val('');
+    show_alert('Cleared!');
+};
 
 function show_alert(message) {
-    $("#alert").html(message).slideDown(200, () => $(this).show()).delay(1000).slideUp(200, () => $(this).hide());
+    $('#alert').html(message).slideDown(200);
+    setTimeout(() => {
+        $('#alert').slideUp(200);
+    }, 1000);
 }
 
 $(document).ready(function () {
@@ -24,7 +44,7 @@ $(document).ready(function () {
         $('#textarea').val(text);
     })
 
-    $('#textarea').on('change keyup keypress', wait(function () {
+    $('#textarea').on('change keyup keypress touchend', wait(function () {
         let text = $('#textarea').val();
         socket.emit('sync text', text)
         console.log(text);
