f0454d558153ae683d2fd3ddd5d9157503f11d37
Joshua Li <joshuali925@gmail.com>
2020-04-24 23:22:22 -0700

    assume on wifi, fix qrcode ip address

--- util.js
+++ util.js
@@ -2,15 +2,21 @@ const fs = require('fs');
 
 function getIPAddress() {
     let interfaces = require('os').networkInterfaces();
-    for (let devName in interfaces) {
-        let iface = interfaces[devName];
-        for (let i = 0; i < iface.length; i++) {
-            let alias = iface[i];
-            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-                return alias.address;
-        }
+    try {
+        return interfaces.wifi0[0].address;
+    } catch (e) {
+        /* handle error */
+        return '0.0.0.0'
     }
-    return '0.0.0.0';
+    // for (let devName in interfaces) {
+    //     let iface = interfaces[devName];
+    //     for (let i = 0; i < iface.length; i++) {
+    //         let alias = iface[i];
+    //         if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+    //             return alias.address;
+    //     }
+    // }
+    // return '0.0.0.0';
 }
 
 function getURL(port) {
